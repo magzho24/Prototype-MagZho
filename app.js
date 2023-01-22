8488e2454604c4b1b4171ab5f02c3b62e9cb537c
magzho24 <magzho24@bergen.org>
2023-01-22 00:12:46 -0500

    commit

---
+++ app.js
@@ -0,0 +1,24 @@
+//set up the server
+const express = require( "express" );
+const app = express();
+const port = 8080;
+
+// define a route for the default home page
+app.get( "/", ( req, res ) => {
+    res.send( "<h1>Hello world!</h1>" );
+} );
+
+// define a route for the stuff inventory page
+app.get( "/counter", ( req, res ) => {
+    res.send( "<h1>This is the stuff inventory page.</h1>" );
+} );
+
+// define a route for the item detail page
+app.get( "/counter/edit", ( req, res ) => {
+    res.send( "<h1>This is the item detail page.</h1>" );
+} );
+
+// start the server
+app.listen( port, () => {
+    console.log(`App server listening on ${ port }. (Go to http://localhost:${ port })` );
+} );
